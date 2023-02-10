d4cf7ffe25ed9568a70815a73b1db48aa95e1d2c
Eudycv07 <eudygonzalezvizcaino@gmail.com>
2023-02-10 14:38:38 -0400

    slider de login

--- src/components/layout/AuthLayout/Side.js
+++ src/components/layout/AuthLayout/Side.js
@@ -2,8 +2,19 @@ import React, { cloneElement } from 'react'
 import { Avatar } from 'components/ui'
 import Logo from 'components/template/Logo'
 import { APP_NAME } from 'constants/app.constant'
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Tdata from "./Tdata"
 
 const Side = ({children, content, ...rest }) => {
+	const settings = {
+		dots: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+	  }
 	return (
 		<div className="grid lg:grid-cols-3 h-full">
 			<div 
@@ -11,16 +22,26 @@ const Side = ({children, content, ...rest }) => {
 				style={{backgroundImage: `url('/img/others/auth-side-bg.jpg')`}}
 			>
 				<Logo mode="dark" />
-				<div >
-					<div className="mb-6 flex items-center gap-4">
-						<Avatar className="border-2 border-white" shape="circle" src="/img/avatars/thumb-10.jpg" />
-						<div className="text-white">
-							<div className="font-semibold text-base">Brittany Hale</div>
-							<span className="opacity-80">CTO, Onward</span>
-						</div>
-					</div>
-					<p className="text-lg text-white opacity-80">Elstar comes with a complete set of UI components crafted with Tailwind CSS, it fulfilled most of the use case to create modern and beautiful UI and application</p>
-				</div>
+				<Slider {...settings}>
+				
+				    {Tdata.map((value, index) => {
+						return(
+							
+							<div key={index} >
+								<div className="mb-6 flex items-center gap-4">
+									<Avatar className="border-2 border-white" shape="circle" src={value.img}/>
+									<div className="text-white">
+										<div className="font-semibold text-base">{value.nom}</div>
+										<span className="opacity-80">{value.desc}</span>
+									</div>
+								</div>
+								<p className="text-lg text-white opacity-80">"{value.phase}</p>
+						
+							</div>
+						)
+						
+					})}
+				</Slider>
 				<span className="text-white">Copyright  &copy;  {`${new Date().getFullYear()}`} <span className="font-semibold">{`${APP_NAME}`}</span> </span>
 			</div>
 			<div className="col-span-2 flex flex-col justify-center items-center bg-white dark:bg-gray-800">
---
+++ src/components/layout/AuthLayout/Tdata.js
@@ -0,0 +1,34 @@
+const Tdata = [
+    {
+      img: "/img/avatars/thumb-2.jpeg",
+      nom: "Oliver Wendell Holmes",
+      desc: "Médico, poeta y erudito estadounidense",
+      phase: "No pongas tu interés en el dinero, pero pon tu dinero a interés",
+    },
+    {
+        img: "/img/avatars/thumb-3.png",
+        nom: "Anonimo ",
+        desc: "",
+        phase: "Ahorrar no es solo guardar, sino saber gastar",
+    },
+    {
+        img: "/img/avatars/thumb-4.jpeg",
+        nom: "Will Rogers",
+        desc: "Humorista, comentarista y actor estadounidense.",
+        phase: "La forma más rápida de doblar tu dinero es plegar los billetes y meterlos en el bolsillo",
+    },
+    {
+        img: "/img/avatars/thumb-5.jpeg",
+        nom: " Benjamin Franklin",
+        desc: "político, científico e inventor estadounidense",
+        phase: "Cuida de los pequeños gastos; un pequeño agujero, hunde un barco",
+    },
+    {
+        img: "/img/avatars/thumb-7.jpg",
+        nom: "Cesare Cantù",
+        desc: "Esta frase del historiador y escritor italiano",
+        phase: "Gasta siempre una moneda menos de lo que ganes",
+    },
+  ]
+  
+  export default Tdata
