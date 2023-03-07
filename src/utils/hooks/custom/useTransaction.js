92cfd9a10b718197e6a4c91cfd81deac2cd9a7aa
Anderson Frias <anderl.friaas@gmail.com>
2023-03-06 20:56:02 -0400

    Hook de Transaction, listo

--- src/utils/hooks/custom/useTransaction.js
+++ src/utils/hooks/custom/useTransaction.js
@@ -1,24 +1,29 @@
 import { useCallback } from 'react'
 import {
     apiGetTransactions,
+    apiGetTransactionById,
+    apiCreateTransaction,
+    apiUpdateTransaction,
+    apiDeleteTransaction,
 } from 'services/TransactionService'
 import { FailedResponse, SuccessResponse } from 'utils/response';
 
+const getLastDayOfMonth = (year, month) => {
+    const date = new Date(year, month + 1, 0);
+    return date.getDate();
+};
+
+const getStartDate = () => {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+};
+
+const getEndDate = () => {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth(), getLastDayOfMonth(date.getFullYear(), date.getMonth()));
+};
+
 function useTransaction() {
-    const getLastDayOfMonth = useCallback((year, month) => {
-        const date = new Date(year, month + 1, 0);
-        return date.getDate();
-    }, []);
-
-    const getStartDate = useCallback(() => {
-        const date = new Date();
-        return new Date(date.getFullYear(), date.getMonth(), 1);
-    }, []);
-
-    const getEndDate = useCallback(() => {
-        const date = new Date();
-        return new Date(date.getFullYear(), date.getMonth(), getLastDayOfMonth(date.getFullYear(), date.getMonth()));
-    }, [getLastDayOfMonth]);
 
     const getTransactions = useCallback(async (filter = '', startDate = getStartDate(), endDate = getEndDate()) => {
         try {
@@ -29,10 +34,50 @@ function useTransaction() {
         } catch (error) {
             return FailedResponse(error);
         }
-    }, [getEndDate, getStartDate]);
+    }, []);
+
+    const getTransactionById = useCallback(async (id) => {
+        try {
+            const resp = await apiGetTransactionById(id);
+            return SuccessResponse(resp.data);
+        } catch (error) {
+            return FailedResponse(error);
+        }
+    }, []);
+
+    const createTransaction = useCallback(async (data) => {
+        try {
+            const resp = await apiCreateTransaction(data);
+            return SuccessResponse(resp.data);
+        } catch (error) {
+            return FailedResponse(error);
+        }
+    }, []);
+
+    const updateTransaction = useCallback(async (id, data) => {
+        try {
+            const resp = await apiUpdateTransaction(id, data);
+            return SuccessResponse(resp.data);
+        } catch (error) {
+            return FailedResponse(error);
+        }
+    }, []);
+
+    const deleteTransaction = useCallback(async (id) => {
+        try {
+            const resp = await apiDeleteTransaction(id);
+            return SuccessResponse(resp.data);
+        } catch (error) {
+            return FailedResponse(error);
+        }
+    }, []);
 
     return {
         getTransactions,
+        getTransactionById,
+        createTransaction,
+        updateTransaction,
+        deleteTransaction,
     }
 }
 
