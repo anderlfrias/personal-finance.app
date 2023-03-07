54b352b2b84474879ab5925a8b1a452e1f363ef9
Eudycv07 <eudygonzalezvizcaino@gmail.com>
2023-03-07 09:40:48 -0400

    Home

---
+++ src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovimientosCard from "../../components/helpers/MovimientosCard"
+
+
+const index = () => {
+  return (
+    <>
+     <MovimientosCard/>
+    </>
+   
+  )
+}
+
+export default index
