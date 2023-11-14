3dbfd0c1c6795b1ca0a787f34af4f5f208dd17a8
Anderson Frias <anderl.friaas@gmail.com>
2023-11-13 22:26:56 -0400

    [Update] Se cambio el preview de la imagen de las evidencias

--- src/components/helpers/Image.js
+++ src/components/helpers/Image.js
@@ -4,7 +4,7 @@ import { HiEye, HiTrash } from 'react-icons/hi'
 function Image({ src, className, onPreview, onDelete }) {
     return (
         <div className={`${className} relative group`}>
-            <img src={src} alt="" className="rounded-md w-full h-full" />
+            <img src={src} alt="" className="rounded-md w-full h-full object-cover" />
             {(onPreview || onDelete) && (
                 <div className="absolute inset-0 rounded-md bg-gray-900/[.7] group-hover:flex hidden text-xl items-center justify-center gap-4">
                     {onPreview && (<HiEye className="cursor-pointer text-white text-2xl" onClick={onPreview} />)}
