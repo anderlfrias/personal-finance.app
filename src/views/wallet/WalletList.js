e0712aef3a3091a4c4ff7ebb7ae36a4e0e400a77
Anderson Frias <anderl.friaas@gmail.com>
2023-11-23 12:01:11 -0400

    [Update] Se muestra el total de todas las cuentas registradas

--- src/views/wallet/WalletList.js
+++ src/views/wallet/WalletList.js
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import useScreenSize from 'utils/hooks/custom/useScreenSize'
 import formatCurrency from "utils/formatCurrency";
 import WalletItem from "./WalletItem";
+import TFoot from "components/ui/Table/TFoot";
 
 const { Tr, Th, Td, THead, TBody } = Table
 
@@ -17,12 +18,19 @@ function WalletList({ wallets, onClickItem }) {
         <>
             {
                 screenWidth < 768 ? (
-                    <div className=' overflow-x-auto'>{
-                        wallets.length > 0 ? wallets.map((item, index) => (
-                            <WalletItem key={index} wallet={item} onClick={() => onClickItem(item)} />
-                        )) :
-                            <div className='text-center text-gray-500'>{t(`${p}.table.empty`)}</div>
-                    }</div>
+                    <div className=' overflow-x-auto'>
+                        { wallets.length > 0 ? (
+                                <>
+                                    {wallets.map((item, index) => <WalletItem key={index} wallet={item} onClick={() => onClickItem(item)} />)}
+                                    <div className='flex justify-end font-bold mt-4'>
+                                        <h6>{t(`${p}.table.total`)}: {formatCurrency(wallets.reduce((total, wallet) => total + wallet.balance, 0))}</h6>
+                                    </div>
+                                </>
+                            ) : (
+                                <div className='text-center text-gray-500'>{t(`${p}.table.empty`)}</div>
+                            )
+                        }
+                    </div>
                 ) :
                 (
                     <Table>
@@ -57,6 +65,18 @@ function WalletList({ wallets, onClickItem }) {
                                 </Tr>
                             }
                         </TBody>
+                        <TFoot>
+                            <Tr>
+                                <Td colSpan={3} className='text-right'>
+                                    <strong>{t(`${p}.table.total`)}</strong>
+                                </Td>
+                                <Td>
+                                    <div className='flex justify-end font-bold'>
+                                        <strong>{formatCurrency(wallets.reduce((total, wallet) => total + wallet.balance, 0))}</strong>
+                                    </div>
+                                </Td>
+                            </Tr>
+                        </TFoot>
                     </Table>
                 )
             }
